a13c7c9cfeb47eb3fec1820349c2c942160015b0
brahimEssaadi <56555807+brahimEssaadi@users.noreply.github.com>
2020-04-27 15:22:02 +0000

    init

--- model-generator.rb
+++ model-generator.rb
@@ -5,12 +5,12 @@ class ModelGenerator < Formula
   desc "Popina model to struct"
   homepage "https://github.com/popina/model-generator"
   url "https://github.com/popina/model-generator.git", :branch => "feature/addArgsAndInstall", :revision => "433d766f999add42d6859413f6447dda87422680"
-  version "1.0.0"
+  version "0.0.1"
   # sha256 ""
   head "https://github.com/popina/model-generator.git" , :branch => "feature/addArgsAndInstall"
   depends_on :xcode => ["11.2.1", :build]
   def install
-    system "make", "install", "prefix=#{prefix}"
+    system "make", "install", "prefix=#{prefix}", "version=#{version}"
 
   end
 
